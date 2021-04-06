2daf27df41a60d95fb93e5f28c0d48b4dc451ffd
Wesley Deal <wesleydeal8@gmail.com>
2021-04-05 22:31:03 -0400

    add spacing, fix errors

--- basicsetup.sh
+++ basicsetup.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-[ -w / ] || echo "ERROR: Invoke this script as root. Exiting." || exit 1
+[ -w / ] || { echo "ERROR: Invoke this script as root. Exiting." ; exit 1 ; }
 
-echo "wd-voidscripts basic setup for void installations"
+echo "\n\nwd-voidscripts basic setup for void installations"
 echo "this will install basic utilities, updates, socklog as a syslog service, and at your request, enable sshd and lock the root account"
 echo "[RETURN] to continue, CTRL+C to quit"
-read
+read nothing
 
-echo "Getting latest updates"
+echo "\n\nGetting latest updates"
 xbps-query -Su
 
-echo "Installing basic utilities"
+echo "\n\nInstalling basic utilities"
 xbps-install -y wget vim git
 
-echo "Installing socklog"
+echo "\n\nInstalling socklog"
 xbps-install -y socklog-void
 ln -s /etc/sv/socklog-unix /var/service/
 ln -s /etc/sv/nanoklogd /var/service/
@@ -21,6 +21,7 @@ sv up socklog-unix
 sv up nanoklogd
 echo "Check logs with svlogtail or from directory /var/log/socklog"
 
+echo "\n\n"
 read -p "Set up an administrative user? [y/N]" yn
 case $yn in
 	[Yy]* ) echo "Does the user already exist?"
